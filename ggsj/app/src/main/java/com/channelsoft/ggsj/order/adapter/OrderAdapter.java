65826c4f869fd85a3d5e3cbcb6fe8da4866a2bd6
chenyg <chenyg@channelsoft.com>
2016-04-22 14:58:47 +0800

    订单列表Item布局

--- ggsj/app/src/main/java/com/channelsoft/ggsj/order/adapter/OrderAdapter.java
+++ ggsj/app/src/main/java/com/channelsoft/ggsj/order/adapter/OrderAdapter.java
@@ -18,14 +18,12 @@ import java.util.List;
 /**
  * Created by dengquan on 16-4-18.
  */
-public class OrderAdapter extends RecyclerView.Adapter
-{
+public class OrderAdapter extends RecyclerView.Adapter {
     private static final int ORDER_DETAIL = 1;
     private static final int LOAD_MORE = 2;
     private Context context;
     private List<OrderListInfo> list = new ArrayList<>();
-    public OrderAdapter(Context context,List<OrderListInfo> list)
-    {
+    public OrderAdapter(Context context,List<OrderListInfo> list) {
         super();
         this.context = context;
         this.list = list;
@@ -38,73 +36,71 @@ public class OrderAdapter extends RecyclerView.Adapter
     }
 
     @Override
-    public int getItemViewType(int position)
-    {
-        if(getItemCount() == position + 1)
-        {
+    public int getItemViewType(int position) {
+        if(getItemCount() == position + 1) {
             return LOAD_MORE;
-        }
-        else
-        {
+        } else {
             return ORDER_DETAIL;
         }
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)
-    {
-        if(holder instanceof OrderViewHolder)
-        {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof OrderViewHolder) {
             OrderViewHolder holder1 = (OrderViewHolder)holder;
-            holder1.textView.setText(list.get(position).getPayPrice()+"元");
-        }
-        else if(holder instanceof FooterViewHolder)
-        {
+            holder1.orderName.setText("顾客  "+list.get(position).getUserPhone());
+            holder1.dishCountAndPrice.setText(list.get(position).getDishList().size()+"道菜   ￥"+list.get(position).getSummaryPrice());
+            String dishnames = "";
+            for(int i = 0;i < list.get(position).getDishList().size(); i ++){
+                dishnames += list.get(position).getDishList().get(i).getDishName()+" ";
+            }
+            holder1.dishNames.setText(dishnames);
+            holder1.price.setText("顾客实付： ￥"+list.get(position).getPayPrice()+"    退款： ￥"+list.get(position).getReturnPrice());
+            holder1.backup.setText("备注："+list.get(position).getRemark());
+        } else if(holder instanceof FooterViewHolder) {
             FooterViewHolder holder1 = (FooterViewHolder) holder;
 //            holder1.textView.setText(context.getResources().getString(R.string.load_more));
         }
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-    {
-        if(viewType == ORDER_DETAIL)
-        {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if(viewType == ORDER_DETAIL) {
             return new OrderViewHolder(LayoutInflater.from(context).inflate(R.layout.item_order,parent,false));
-        }
-        else
-        {
+        } else {
             return new FooterViewHolder(LayoutInflater.from(context).inflate(R.layout.footer_view,parent,false));
         }
     }
 
-    public void addHeaderData(List<OrderListInfo> orderList)
-    {
+    public void addHeaderData(List<OrderListInfo> orderList) {
         Toast.makeText(context,"header data  ",Toast.LENGTH_LONG).show();
         list = orderList;
         this.notifyDataSetChanged();
     }
 
-    public void addFooterData(List<OrderListInfo> orderList)
-    {
+    public void addFooterData(List<OrderListInfo> orderList) {
         Toast.makeText(context,"footer data  ",Toast.LENGTH_LONG).show();
         list = orderList;
         this.notifyDataSetChanged();
     }
 
-    class OrderViewHolder extends RecyclerView.ViewHolder
-    {
-        private TextView textView;
-        public OrderViewHolder(View itemView)
-        {
+    class OrderViewHolder extends RecyclerView.ViewHolder {
+        private TextView orderName;
+        private TextView dishCountAndPrice;
+        private TextView dishNames;
+        private TextView price;
+        private TextView backup;
+        public OrderViewHolder(View itemView) {
             super(itemView);
-            textView = (TextView) itemView.findViewById(R.id.orderName);
+            orderName = (TextView) itemView.findViewById(R.id.orderName);
+            dishCountAndPrice = (TextView) itemView.findViewById(R.id.dishCountAndPrice);
+            dishNames = (TextView) itemView.findViewById(R.id.dishNames);
+            price = (TextView) itemView.findViewById(R.id.price);
+            backup = (TextView) itemView.findViewById(R.id.backup);
         }
     }
 
-    class FooterViewHolder extends RecyclerView.ViewHolder
-    {
-
+    class FooterViewHolder extends RecyclerView.ViewHolder {
         public FooterViewHolder(View itemView)
         {
             super(itemView);
